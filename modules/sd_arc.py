99c8115660615d14ff66e02fdf336469ce6ffabe
maoxianren <maoxianren@example.com>
2023-08-02 10:05:16 +0000

    feat: arc support controlnet and sdxl

--- modules/sd_arc.py
+++ modules/sd_arc.py
@@ -292,7 +292,6 @@ class SpecifiedCache:
         try:
             start_time = time.time()
             need_size = (p.height * p.width /(512*512) - 1) * (self.size_base + self.batch_base) + 4 # not include model size
-            print(p)
             for item in p.script_args:
                 if ("controlnet" in str(type(item)).lower() and item.enabled) or (type(item) == dict and item.get("model") is not None):
                         need_size += 0.7   
